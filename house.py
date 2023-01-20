26992aa56fdba50da4a4c35dc4a928d4293ac421
hmccann3 <hollymmccann@gmail.com>
2023-01-20 12:22:27 -0800

    moved bounding box

--- house.py
+++ house.py
@@ -9,6 +9,13 @@ HOUSE_WIDTH = 700
 
 t = turtle.Turtle()
 
+t.penup()
+t.left(180)
+t.forward(550)
+t.left(90)
+t.forward(400)
+t.left(90)
+t.pendown()
 t.forward(BOUNDING_WIDTH)
 t.left(90)
 t.forward(BOUNDING_HEIGHT)
