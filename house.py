376847b1b7faa44cec57a3ca0dda44ac197307fc
Kristy Carpenter <kcarp@stanford.edu>
2023-01-20 12:09:09 -0800

    bounding box

---
+++ house.py
@@ -0,0 +1,18 @@
+import turtle
+
+BOUNDING_HEIGHT = 800
+BOUNDING_WIDTH = 1200
+HOUSE_HEIGHT = 500
+HOUSE_WIDTH = 700
+
+
+
+t = turtle.Turtle()
+
+t.forward(BOUNDING_WIDTH)
+t.left(90)
+t.forward(BOUNDING_HEIGHT)
+t.left(90)
+t.forward(BOUNDING_WIDTH)
+t.left(90)
+t.forward(BOUNDING_HEIGHT)
