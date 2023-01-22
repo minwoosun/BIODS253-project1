b85888331ba41689454263431165cd57b948beaa
Min Woo Sun <minwoosun55@gmail.com>
2023-01-22 15:25:40 -0800

    fill bounding box with lightblue && draw clouds

--- house.py
+++ house.py
@@ -10,6 +10,7 @@ TREE_WIDTH = 30
 
 
 def draw_bounding_box(t):
+    t.fillcolor('lightblue')
     t.penup()
     t.left(180)
     t.forward(550)
@@ -17,6 +18,7 @@ def draw_bounding_box(t):
     t.forward(400)
     t.left(90)
     t.pendown()
+    t.begin_fill()
     t.forward(BOUNDING_WIDTH)
     t.left(90)
     t.forward(BOUNDING_HEIGHT)
@@ -24,6 +26,7 @@ def draw_bounding_box(t):
     t.forward(BOUNDING_WIDTH)
     t.left(90)
     t.forward(BOUNDING_HEIGHT)
+    t.end_fill()
 
 def draw_house(t):
     t.penup()
@@ -122,12 +125,56 @@ def draw_all_trees(t):
     # draw left tree 
     draw_tree(t)
 
+def draw_circle(t, rad, col="white"):
+    """Draw a circle.
+    Params:
+        t: the Tutle object
+        rad: radius of the circle
+        col: color of the circle
+    """
+    t.color(col,col)
+    t.begin_fill()
+    t.circle(rad)
+    t.end_fill()
+
+
+def draw_cloud(t, rad=30, col="white", x_start=0, y_start=0):
+    """Draw a cloud. 
+    Params:
+        t: the Tutle object
+        rad: radius of the circle
+        col: color of the circle
+        x_start: x coordinate for starting pos
+        y_start: y coordinate for starting pos
+    """
+    t.penup()
+    t.goto(x_start, y_start)
+    t.pendown()
+    draw_circle(t, rad, col)
+    t.right(90)
+    draw_circle(t, rad, col)
+    t.right(90)
+    draw_circle(t, rad, col)
+    t.right(90)
+    draw_circle(t, rad, col)
+  
+def draw_all_clouds(t):
+    """Draw two clouds.
+    Params:
+        t: the Tutle object.
+    """
+    draw_cloud(t, x_start=400, y_start=250)
+    draw_cloud(t, x_start=450, y_start=250)
+    draw_cloud(t, x_start=350, y_start=250)
+    draw_cloud(t, x_start=-400, y_start=250)
+    draw_cloud(t, x_start=-350, y_start=250)
 
 def main():
     t = turtle.Turtle()
     draw_bounding_box(t)
     draw_house(t)
     draw_all_trees(t)
+    draw_all_clouds(t)
     turtle.done()
 
 if __name__ == "__main__":
